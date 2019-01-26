5cbf2b3cdbfd07c600517bda2e934f754549fb48
Ollie Nye <ollie@nyemail.co.uk>
2019-01-26 18:49:40 +0000

    Add autoload path for Steam API

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,7 @@ module Api
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.autoload_paths << "#{Rails.root}/app/services/steam_api_service"
   end
 end
